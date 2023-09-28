56707fac163396cfb451f258183961c6003e637a
aman4uas <aman4u.as@gmail.com>
2023-09-28 20:10:59 +0530

    update

--- client/index.js
+++ client/index.js
@@ -1,61 +1,63 @@
-let baseURL = "http://localhost:3000/result/";  //Base url for get request to server
+let baseURL = "https://yt-tool-extension-server.onrender.com/result/"; //Base url for get request to server
 const cookiesTime = 60 * 60 * 24 * 30 * 3; //Approx three months (time in sec)
 let mode = "light"; //Initailly set to light mode
 init();
 
 function getCookie() {
-  chrome.cookies.get({
-    url: "https://youtube.com",
-    name: "mode"
-  }, (cookie) => {
-    if (chrome.runtime.lastError) 
+  chrome.cookies.get(
     {
-      console.error("Error getting cookie:", chrome.runtime.lastError);
-      mode = "light";
-    } 
-    else 
-    {
-      if (cookie) mode = cookie.value;
-      else mode = "light";
+      url: "https://youtube.com",
+      name: "mode",
+    },
+    (cookie) => {
+      if (chrome.runtime.lastError) {
+        console.error("Error getting cookie:", chrome.runtime.lastError);
+        mode = "light";
+      } else {
+        if (cookie) mode = cookie.value;
+        else mode = "light";
+      }
+      if (mode === "dark") addDarkClassToAllElements();
     }
-    if (mode === "dark") addDarkClassToAllElements();
-  });
+  );
 }
 
 function setCookie(str) {
-  chrome.cookies.set({
-    url: "https://youtube.com",
-    name: "mode",
-    value: str,
-    expirationDate: (new Date().getTime() / 1000) + cookiesTime,
-    secure: true, // Set to true for HTTPS
-    httpOnly: true, // Restrict access to JavaScript
-  }, (cookie) => {
-    if (chrome.runtime.lastError) console.error("Error setting cookie:", chrome.runtime.lastError);
-    else console.log("Cookie has been set:", cookie);
-  });
+  chrome.cookies.set(
+    {
+      url: "https://youtube.com",
+      name: "mode",
+      value: str,
+      expirationDate: new Date().getTime() / 1000 + cookiesTime,
+      secure: true, // Set to true for HTTPS
+      httpOnly: true, // Restrict access to JavaScript
+    },
+    (cookie) => {
+      if (chrome.runtime.lastError)
+        console.error("Error setting cookie:", chrome.runtime.lastError);
+      else console.log("Cookie has been set:", cookie);
+    }
+  );
 }
 
 function addDarkClassToAllElements() {
-  const elements = document.getElementsByTagName('*');
-  for (let i = 0; i < elements.length; i++) elements[i].classList.add('dark');
+  const elements = document.getElementsByTagName("*");
+  for (let i = 0; i < elements.length; i++) elements[i].classList.add("dark");
 }
 
 function removeDarkClassFromAllElements() {
-  const elements = document.getElementsByTagName('*');
-  for (let i = 0; i < elements.length; i++) elements[i].classList.remove('dark');
+  const elements = document.getElementsByTagName("*");
+  for (let i = 0; i < elements.length; i++)
+    elements[i].classList.remove("dark");
 }
 
 function toggleMode() {
   hide("content");
-  if (mode === "light") 
-  {
+  if (mode === "light") {
     mode = "dark";
     setCookie("dark");
     addDarkClassToAllElements();
-  } 
-  else 
-  {
+  } else {
     mode = "light";
     setCookie("light");
     removeDarkClassFromAllElements();
@@ -63,27 +65,24 @@ function toggleMode() {
   refresh();
 }
 
-
-
-
 function show(params) {
-  document.getElementById(params).classList.remove('hidden');
+  document.getElementById(params).classList.remove("hidden");
   document.getElementById(params).classList.add("flex");
 }
 
 function hide(params) {
-  document.getElementById(params).classList.remove('flex');
+  document.getElementById(params).classList.remove("flex");
   document.getElementById(params).classList.add("hidden");
 }
 
 function process() {
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
     let url = tabs[0].url;
     let ans = isYtUrl(url);
     if (ans === true) calculateTime(url);
-    else 
-    {
-      document.getElementById("error_msg").innerHTML = "Not a YouTube page...!!";
+    else {
+      document.getElementById("error_msg").innerHTML =
+        "Not a YouTube page...!!";
       hide("loader");
       show("error");
     }
@@ -91,7 +90,7 @@ function process() {
 }
 
 function isYtUrl(inputString) {
-  console.log(inputString)
+  console.log(inputString);
   const regex = /^https?:\/\/(www\.|m\.)?youtube\.com/;
   let ans = regex.test(inputString);
   console.log("isYTURL", ans);
@@ -107,13 +106,11 @@ function getYTPlaylistID(playlistLink) {
 
 function calculateTime(url) {
   let id = null;
-  try 
-  {
+  try {
     id = getYTPlaylistID(url);
-  } 
-  catch 
-  {
-    document.getElementById("error_msg").innerHTML = "Not a YouTube playlist page...<br>Please visit and try again !!"
+  } catch {
+    document.getElementById("error_msg").innerHTML =
+      "Not a YouTube playlist page...<br>Please visit and try again !!";
     hide("loader");
     show("error");
     return;
@@ -122,24 +119,20 @@ function calculateTime(url) {
 }
 
 async function getTime(id) {
-
   let url, response;
   url = baseURL + id;
-  try 
-  {
+  try {
     response = await fetch(url);
     response = await response.json();
-  } 
-  catch(error) 
-  {
+  } catch (error) {
     console.log(error);
-    document.getElementById("error_msg").innerHTML = "Uhh... Something went wrong <br>Please try again later !!";
+    document.getElementById("error_msg").innerHTML =
+      "Uhh... Something went wrong <br>Please try again later !!";
     hide("loader");
     show("error");
     return;
   }
-  if (!response.ok) 
-  {
+  if (!response.ok) {
     document.getElementById("error_msg").innerHTML = response.message;
     hide("loader");
     show("error");
@@ -167,11 +160,13 @@ function refresh() {
 
 async function init() {
   let element = document.getElementsByClassName("mode-btn");
-  for (let i = 0; i < element.length; i++) element[i].addEventListener("click", toggleMode);
+  for (let i = 0; i < element.length; i++)
+    element[i].addEventListener("click", toggleMode);
 
   element = document.getElementsByClassName("reload");
-  for (let i = 0; i < element.length; i++) element[i].addEventListener("click", refresh);
-  
+  for (let i = 0; i < element.length; i++)
+    element[i].addEventListener("click", refresh);
+
   getCookie();
   refresh();
 }
